12aee26b604d9a32f6ac77bd955eba33ceebc4ba
Katarina Vucic <kavucic@icloud.com>
2025-01-18 02:43:19 -0500

    feature: added apple macintosh model

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react';
+import dynamic from 'next/dynamic';
+
+// imports the App component from the App.jsx file 
+// ssr: false ensures that the component is not server-side rendered
+const AppleMacintosh = dynamic(() => import('./components/AppleMacintosh'), { ssr: false });
+
+export default function HomePage() {
+  return (
+    <React.StrictMode>
+      <AppleMacintosh />
+    </React.StrictMode>
+  );
+}
