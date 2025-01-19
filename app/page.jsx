db43c95ec4958c2ae2318d8caf2cf9d9492b1d51
Anushka <anushka.sethi1809@gmail.com>
2025-01-19 10:19:53 +0530

    Ai functionality added

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Splash from "./components/splash";
 import Main from "./components/main";
 import Landing from "./components/landing";
-
+// import CohereAI from './components/CohereAi';
 // imports the App component from the App.jsx file 
 // ssr: false ensures that the component is not server-side rendered
 const AppleMacintosh = dynamic(() => import('./components/DesktopComputer'), { ssr: false });
@@ -16,7 +16,7 @@ export default function Home() {
       {/* <Splash />
       <Main /> */}
       <Landing />
-
+      {/* <CohereAI /> */}
     </>
   );
 }
