1dd258cb745a660d201ef310faaf3d7be2c78f4d
akcljt <139833916+akcljt@users.noreply.github.com>
2025-01-18 21:24:23 -0500

    changed favicon!!

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import Splash from "./components/splash";
 import Main from "./components/main";
 import Landing from "./components/landing";
+import Head from 'next/head';
+
 
 // imports the App component from the App.jsx file 
 // ssr: false ensures that the component is not server-side rendered
@@ -13,9 +15,14 @@ const AppleMacintosh = dynamic(() => import('./components/DesktopComputer'), { s
 export default function Home() {
   return (
     <>
+    <Head>
+      <link rel="icon" type="image/x-icon" size="32x32" href="/favicon.ico" />
+    </Head>
+    <div>
       {/* <Splash />
       <Main /> */}
       <Landing />
+    </div>
 
     </>
   );
