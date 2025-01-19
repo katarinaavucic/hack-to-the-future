acd77844b744606601b617586d4de090241a51c5
Tyler Steptoe <tytysteptoe@gmail.com>
2025-01-18 19:05:02 -0500

    added text functionality for desktop

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import Splash from "./components/splash";
 
 // imports the App component from the App.jsx file 
 // ssr: false ensures that the component is not server-side rendered
-const AppleMacintosh = dynamic(() => import('./components/PDP1Computer'), { ssr: false });
+const AppleMacintosh = dynamic(() => import('./components/DesktopComputer'), { ssr: false });
 
 export default function Home() {
   return (
